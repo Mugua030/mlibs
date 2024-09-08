909f12d3429e4fbd8be0e879c6474a04f5e1b7f4
C.Martin <email.mugua@gmail.com>
2024-09-08 09:30:45 +0800

    pool v2

--- pkg/pool.go
+++ pkg/pool.go
@@ -1,10 +1,30 @@
 package pkg
 
 import (
+	"fmt"
+	"os"
 	"runtime"
+	"strconv"
 	"sync"
 )
 
+func init() {
+	// default max
+	MAX_TASK_QUEUE = 20
+	qNum := os.Getenv("MAX_TASK_QUEUE")
+	if qNum != "" {
+		num, err := strconv.Atoi(qNum)
+		if err != nil {
+			panic("fail get queue num")
+		}
+		MAX_TASK_QUEUE = num
+	}
+	TaskQueue = make(chan Task, MAX_TASK_QUEUE)
+}
+
+var MAX_TASK_QUEUE int
+
+// task
 type Task interface {
 	Execute()
 	GetResult() string
@@ -79,4 +99,91 @@ func (t *TesterTask) Execute() {
 
 func (t *TesterTask) GetResult() string {
 	return t.Result
+}
+
+// => GPM
+var TaskQueue chan Task
+
+type QuitSignal = struct{}
+type ChanEmbedChanTask = chan chan Task
+
+type Worker struct {
+	WorkerPool  chan chan Task
+	TaskChannel chan Task
+	quit        chan QuitSignal
+}
+
+func NewWorker(wkPool ChanEmbedChanTask) Worker {
+	return Worker{
+		WorkerPool:  wkPool,
+		TaskChannel: make(chan Task),
+		quit:        make(chan QuitSignal),
+	}
+}
+
+// real processer for task
+func (w Worker) Start() {
+	//var hungryNum int = 0
+	for {
+		// 1.1 first put task channel for dispatch can dispatch task
+		// 1.1 work run early than dispatch
+		// 2.put the channel to the worker pool when task execute finish
+		w.WorkerPool <- w.TaskChannel
+
+		select {
+		case task := <-w.TaskChannel:
+			task.Execute()
+			fmt.Printf("worker result: %v\n", task.GetResult())
+		case <-w.quit:
+			fmt.Println("worker quit !")
+			return
+		}
+	}
+}
+
+func (w Worker) Stop() {
+	w.quit <- QuitSignal{}
+}
+
+// Dispach
+type Dispatcher struct {
+	WorkerPool ChanEmbedChanTask
+	maxWorker  int
+}
+
+func NewDispatcher(maxWorkers int) *Dispatcher {
+	pool := make(ChanEmbedChanTask, maxWorkers)
+	return &Dispatcher{
+		WorkerPool: pool,
+		maxWorker:  maxWorkers,
+	}
+}
+
+func (d *Dispatcher) Run() {
+
+	for i := 0; i < d.maxWorker; i++ {
+		worker := NewWorker(d.WorkerPool)
+		go worker.Start()
+	}
+
+	go d.dispatch()
+}
+
+func (d *Dispatcher) dispatch() {
+
+	//for task := range TaskQueue {
+	for {
+		if task, ok := <-TaskQueue; ok {
+			go func(tsk Task) {
+				taskChannel := <-d.WorkerPool
+
+				taskChannel <- tsk
+			}(task)
+		} else {
+			// need to quit
+			fmt.Println("[TODO] need to quite !")
+			return
+		}
+
+	}
 }
--- pkg/pool_test.go
+++ pkg/pool_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestPool(t *testing.T) {
@@ -29,4 +30,44 @@ func TestPool(t *testing.T) {
 	// out
 	fmt.Println(product.GetResult())
 	fmt.Println(code.GetResult())
+}
+
+func TestTaskPool(t *testing.T) {
+	go func() {
+		for i := 0; i <= 18; i++ {
+			tsk := NewBigDataTask(i)
+			TaskQueue <- tsk
+		}
+	}()
+
+	dspch := NewDispatcher(3)
+	dspch.Run()
+
+	select {}
+	//fmt.Println("task finished !")
+}
+
+// for test task
+type BigDataTask struct {
+	Id     int
+	Name   string
+	Result string
+}
+
+func NewBigDataTask(id int) *BigDataTask {
+	return &BigDataTask{
+		Id:     id,
+		Name:   "",
+		Result: "",
+	}
+}
+
+func (b *BigDataTask) Execute() {
+	//fmt.Println("BigDataTask execute Done")
+	time.Sleep(time.Second * 1)
+	ret := fmt.Sprintf("BigDataTask[%d] Execute Done", b.Id)
+	b.Result = ret
+}
+func (b *BigDataTask) GetResult() string {
+	return b.Result
 }
